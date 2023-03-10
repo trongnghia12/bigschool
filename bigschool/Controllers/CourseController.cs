94c36b3a8d1f00a0d79301ad9087042a71cf0549
Admin <Admin@DESKTOP-T0V7909>
2023-03-10 14:24:35 +0700

    Add project files.

---
+++ bigschool/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using bigschool.Models;
+using bigschool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bigschool.Controllers
+{
+    public class CourseController : Controller  
+    {
+        private readonly ApplicationDbContext _dbContext;
+            public CourseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
